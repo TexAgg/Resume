24f52ec9ba3b823a5e33510d06f7629962fa74f6
Matt Gaikema <mgaikema1@gmail.com>
2016-10-21 22:26:35 -0500

    Add date generated

--- render.py
+++ render.py
@@ -5,21 +5,30 @@
 import jinja2
 import os
 import json
+import time
+from datetime import date
 from jinja2 import Template
+
+# Returns the current date.
+# https://docs.python.org/2/library/datetime.html
+def today():
+    d = date.fromtimestamp(time.time())
+    return d.isoformat()
 
 def get_skills():
     f = open("data/skills.json")
     return json.load(f)
 
+# http://jinja.pocoo.org/docs/dev/api/
 latex_jinja_env = jinja2.Environment(
     block_start_string = '\BLOCK{',
     block_end_string = '}',
     variable_start_string = '\VAR{',
     variable_end_string = '}',
-    comment_start_string = '\#{',
-    comment_end_string = '}',
+    #comment_start_string = '\#{',
+    #comment_end_string = '}',
     line_statement_prefix = "-%",
-    line_comment_prefix = '%#',
+    #line_comment_prefix = '%#',
     trim_blocks = True,
     autoescape = False,
 	loader = jinja2.FileSystemLoader(os.path.abspath('.'))
@@ -29,13 +38,13 @@ latex_jinja_env = jinja2.Environment(
 skills = get_skills()
 
 resume_template = latex_jinja_env.get_template('resume.template.tex')
-resume_str = resume_template.render(languages = skills['languages'], software = skills['software'])
+resume_str = resume_template.render(languages = skills['languages'], software = skills['software'], today = today)
 with open("resume.tex", "w+") as f:
     f.write(resume_str)
 print("resume.tex written.")
 
 cv_template = latex_jinja_env.get_template('cv.template.tex')
-cv_str = cv_template.render(languages = skills['languages'], software = skills['software'])
+cv_str = cv_template.render(languages = skills['languages'], software = skills['software'], today = today)
 with open("cv.tex", "w+") as f:
     f.write(cv_str)
 print("cv.tex written.")
